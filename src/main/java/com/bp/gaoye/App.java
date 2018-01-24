1a79d03ce71651e84cae51f20f704a7d2ceb0eda
gaoyee <ye_gao@163.com>
2018-01-24 17:17:07 +0800

    second

---
+++ src/main/java/com/bp/gaoye/App.java
@@ -0,0 +1,21 @@
+package com.bp.gaoye;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+
+{
+
+    /**
+     * 仅仅是测试一下
+     */
+    public void test(){
+        System.out.println("true = " + true);
+    }
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ tools/src/main/java/GaoyeTest.java
@@ -0,0 +1,13 @@
+/**
+ * Created by GY on 2018/1/24.
+ */
+
+/**
+ * just test
+ */
+public class GaoyeTest {
+    public void print() {
+        String hl = "Hello World";//1
+        System.out.println(hl);
+    }
+}
