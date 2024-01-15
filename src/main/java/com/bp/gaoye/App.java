eaa896e88a356daf47de9751a2a2d0f37b8a8c9e
gary <gary@jiemogame.com>
2024-01-15 12:22:49 +0800

    test2

--- src/main/java/com/bp/gaoye/App.java
+++ src/main/java/com/bp/gaoye/App.java
@@ -16,6 +16,6 @@ public class App
     }
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! 2" );
+        System.out.println( "Hello World! i am ok" );
     }
 }
